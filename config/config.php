d1b16321e2b366d2da1db749758d383acac9bd55
kiriakivel <kiriaki.vel@gmail.com>
2019-04-17 20:49:43 +0300

    Initial Commit

---
+++ config/config.php
@@ -0,0 +1,41 @@
+<?php
+/* ========================================================
+ * OpeneClass 2.2 configuration file
+ * Automatically created by install on 2019-04-17 18:57
+ * ======================================================== */
+
+$urlServer = "http://bug_injection.csec.gr/";
+$urlAppend = "";
+$webDir    = "/var/www/eclass/bug_injection.csec.gr/" ;
+
+$mysqlServer = "127.0.0.1:11018";
+$mysqlUser = "root";
+$mysqlPassword = "Tnwr5IbyA7FF/TxypkHaooRY";
+$mysqlMainDb = "eclass";
+$phpMyAdminURL = "../admin/mysql/";
+$phpSysInfoURL = "../admin/sysinfo/";
+$emailAdministrator = "webmaster@localhost";
+$administratorName = "Διαχειριστής";
+$administratorSurname = "Πλατφόρμας";
+$siteName = "Open eClass";
+
+$telephone = "+30 2xx xxxx xxx";
+$fax = "";
+$emailhelpdesk = "";
+
+$language = "greek";
+
+$Institution = "Ακαδημαϊκό Διαδίκτυο GUNet ";
+$InstitutionUrl = "http://www.gunet.gr/";
+$postaddress = "";
+
+$have_latex = FALSE;
+$close_user_registration = FALSE;
+
+$persoIsActive = TRUE;
+$durationAccount = "126144000";
+
+define("UTF8", true);
+
+
+$encryptedPasswd = true;
